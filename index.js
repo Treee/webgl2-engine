834e91f0692b2f1b282ea9c9991c60c4882275a1
Tree <tree.crypto@gmail.com>
2018-05-25 21:08:10 -0600

    adding exports to index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Geometries
+export { BoxGeometry } from './src/geometries/box-geometry';
+
+// Math
 export { Vec3 } from './src/math/vec3';
 export { Vec4 } from './src/math/vec4';
 export { Mat3 } from './src/math/mat3';
+
+// Shaders
+export { ShaderProgram } from './src/renderer/shaders/shader-program';
+
+export { RendererEngine } from './src/renderer/renderer-engine';
