052c979b677c1edd6f667e1777f352949068db00
Tree <tree.crypto@gmail.com>
2018-05-25 21:54:57 -0600

    replaced src with dist in index js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Geometries
-export { BoxGeometry } from './src/geometries/box-geometry';
+export { BoxGeometry } from './dist/geometries/box-geometry';
 
 // Math
-export { Vec3 } from './src/math/vec3';
-export { Vec4 } from './src/math/vec4';
-export { Mat3 } from './src/math/mat3';
+export { Vec3 } from './dist/math/vec3';
+export { Vec4 } from './dist/math/vec4';
+export { Mat3 } from './dist/math/mat3';
 
 // Shaders
-export { ShaderProgram } from './src/renderer/shaders/shader-program';
+export { ShaderProgram } from './dist/renderer/shaders/shader-program';
 
-export { RendererEngine } from './src/renderer/renderer-engine';
+export { RendererEngine } from './dist/renderer/renderer-engine';
