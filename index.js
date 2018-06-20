19fbc254c27d0fe72cfe0ebebbf09417605b2bcc
Tree <tree.crypto@gmail.com>
2018-06-20 13:18:56 -0600

    adding point to indexjs export so it is now accessible

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Geometries
 export { BoxGeometry } from './dist/geometries/box-geometry';
+export { Point2D } from './dist/geometries/point-2d';
 
 // Math
 export { Vec3 } from './dist/math/vec3';
