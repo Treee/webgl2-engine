e9094c9b46da249ee0af28d3b9db6f26d34fc20f
Treee <jonathon.lindemann@gmail.com>
2018-11-20 15:11:09 -0700

    pulling path finding out of the grid data structure

---
+++ src/algorithms/a-start-spec.ts
@@ -0,0 +1,71 @@
+import { AStar } from './a-star';
+import { Grid2D } from '../data-structures/grid-2d';
+import { Grid2DCell } from '../data-structures/grid-2d-cell';
+
+describe('A Star', () => {
+  let testAStar: AStar;
+  let testGrid: Grid2D;
+
+  let gridRows: number = 5;
+  let gridCols: number = 5;
+
+  let sampleGrid = 'sooxo\noxoxo\nooooo\nxoxxx\noooof';
+  // sooxo
+  // oxoxo 
+  // ooooo 
+  // xoxxx 
+  // oooof
+
+  beforeEach(() => {
+    testAStar = new AStar();
+    testGrid = new Grid2D();
+    testGrid.initializeGrid(gridRows, gridCols);
+  });
+
+  it('knows the best option to take out of a list', () => {
+    const cell0 = new Grid2DCell(0, 'blocked');
+    const cell1 = new Grid2DCell(1, 'blocked');
+    const cell2 = new Grid2DCell(2, 'blocked');
+    const expectedBestOption = new Grid2DCell(3, 'open');
+    const cell4 = new Grid2DCell(4, 'blocked');
+    const cell5 = new Grid2DCell(5, 'blocked');
+    const cell6 = new Grid2DCell(6, 'blocked');
+    const openCells = [cell0, cell1, cell2, expectedBestOption, cell4, cell5, cell6]
+    const actualBestOption = testAStar.getBestCellOption(openCells);
+    expect(actualBestOption).toEqual(expectedBestOption);
+  });
+
+  it('shows the heuristic cost estimate (straight path to target)', () => {
+    let expectedEstimate = 5;
+    let actualEstimate = testAStar.heuristicCostEstimate(testGrid.grid[1], testGrid.grid[14], gridRows, gridCols);
+    expect(actualEstimate).toEqual(expectedEstimate);
+  });
+
+  it('shows the heuristic cost estimate for an upside down path', () => {
+    let expectedEstimate = 5;
+    let actualEstimate = testAStar.heuristicCostEstimate(testGrid.grid[21], testGrid.grid[14], gridRows, gridCols);
+    expect(actualEstimate).toEqual(expectedEstimate);
+
+  });
+  it('shows the heuristic cost estimate for an equilateral path', () => {
+    let expectedEstimate = 4.242640687119285;
+    let actualEstimate = testAStar.heuristicCostEstimate(testGrid.grid[10], testGrid.grid[22], gridRows, gridCols);
+    expect(actualEstimate).toEqual(expectedEstimate);
+  });
+
+  it('finds a path between two cells', () => {
+    testGrid.loadGrid(sampleGrid);
+    testGrid.connectGridCells();
+    const expectedPath: Grid2DCell[] = [testGrid.grid[24], testGrid.grid[23], testGrid.grid[22], testGrid.grid[21], testGrid.grid[16], testGrid.grid[11], testGrid.grid[10], testGrid.grid[5], testGrid.grid[0]];
+    const actualPath = testAStar.aStar(testGrid.startingPoint, testGrid.finishingPoint, gridRows, gridCols);
+    expect(actualPath).toEqual(expectedPath);
+  });
+
+  it('finds a path between two other cells', () => {
+    testGrid.loadGrid(sampleGrid);
+    testGrid.connectGridCells();
+    const expectedPath: Grid2DCell[] = [testGrid.grid[21], testGrid.grid[16], testGrid.grid[11], testGrid.grid[10], testGrid.grid[5], testGrid.grid[0]];
+    const actualPath = testAStar.aStar(testGrid.startingPoint, testGrid.grid[21], gridRows, gridCols);
+    expect(actualPath).toEqual(expectedPath);
+  });
+});
--- src/data-structures/grid-2d.ts
+++ src/data-structures/grid-2d.ts
@@ -67,108 +67,108 @@ export class Grid2D {
     }
   }
 
-  public aStar(startingCell: Grid2DCell, finishingCell: Grid2DCell) {
-    // The set of nodes already evaluated
-    let closedSet: Grid2DCell[] = [];
-    // The set of currently discovered nodes that are not evaluated yet.
-    // Initially, only the start node is known.
-    let openSet: Grid2DCell[] = [startingCell];
-    // For each node, which node it can most efficiently be reached from.
-    // If a node can be reached from many nodes, cameFrom will eventually contain the
-    // most efficient previous step.
-    let cameFrom = new Map();
-    // For each node, the cost of getting from the start node to that node.
-    let gScore = new Map();
-    // The cost of going from start to start is zero.
-    gScore.set(startingCell, 0);
-    // For each node, the total cost of getting from the start node to the goal
-    // by passing by that node. That value is partly known, partly heuristic.
-    let fScore = new Map();
-    // For the first node, that value is completely heuristic.
-    fScore.set(startingCell, this.heuristicCostEstimate(startingCell, finishingCell));
-    let current: Grid2DCell;
-    // while there are nodes to be looked at
-    while (openSet.length > 0) {
-      // console.log('``````````````````````Looooooooop begin ```````````````````````');
-      // console.log(`open set is still populated`, openSet);
-      // best cell in the open set to move too
-      current = this.getBestCellOption(openSet);
-      if (current === finishingCell) {
-        console.log('finished');
-        return this.reconstructPath(cameFrom, current);
-        // break;
-      }
+  // public aStar(startingCell: Grid2DCell, finishingCell: Grid2DCell) {
+  //   // The set of nodes already evaluated
+  //   let closedSet: Grid2DCell[] = [];
+  //   // The set of currently discovered nodes that are not evaluated yet.
+  //   // Initially, only the start node is known.
+  //   let openSet: Grid2DCell[] = [startingCell];
+  //   // For each node, which node it can most efficiently be reached from.
+  //   // If a node can be reached from many nodes, cameFrom will eventually contain the
+  //   // most efficient previous step.
+  //   let cameFrom = new Map();
+  //   // For each node, the cost of getting from the start node to that node.
+  //   let gScore = new Map();
+  //   // The cost of going from start to start is zero.
+  //   gScore.set(startingCell, 0);
+  //   // For each node, the total cost of getting from the start node to the goal
+  //   // by passing by that node. That value is partly known, partly heuristic.
+  //   let fScore = new Map();
+  //   // For the first node, that value is completely heuristic.
+  //   fScore.set(startingCell, this.heuristicCostEstimate(startingCell, finishingCell));
+  //   let current: Grid2DCell;
+  //   // while there are nodes to be looked at
+  //   while (openSet.length > 0) {
+  //     // console.log('``````````````````````Looooooooop begin ```````````````````````');
+  //     // console.log(`open set is still populated`, openSet);
+  //     // best cell in the open set to move too
+  //     current = this.getBestCellOption(openSet);
+  //     if (current === finishingCell) {
+  //       console.log('finished');
+  //       return this.reconstructPath(cameFrom, current);
+  //       // break;
+  //     }
 
-      let currentIndex = openSet.indexOf(current);
-      openSet.splice(currentIndex, 1);
+  //     let currentIndex = openSet.indexOf(current);
+  //     openSet.splice(currentIndex, 1);
 
-      closedSet.push(current);
+  //     closedSet.push(current);
 
-      // for each neighbor
-      current.connectedCells.forEach((connectedCell) => {
-        // remove any neighbors that have already been evaluated
-        if (closedSet.indexOf(connectedCell) > 0) {
-          return;
-        }
-        // remove any neighbors that are blocked
-        if (connectedCell.cellType === 'blocked') {
-          return;
-        }
-        // The distance from start to a neighbor
-        let tentative_gScore = gScore.get(current) + connectedCell.getMovementWeight();
+  //     // for each neighbor
+  //     current.connectedCells.forEach((connectedCell) => {
+  //       // remove any neighbors that have already been evaluated
+  //       if (closedSet.indexOf(connectedCell) > 0) {
+  //         return;
+  //       }
+  //       // remove any neighbors that are blocked
+  //       if (connectedCell.cellType === 'blocked') {
+  //         return;
+  //       }
+  //       // The distance from start to a neighbor
+  //       let tentative_gScore = gScore.get(current) + connectedCell.getMovementWeight();
 
-        // Discover a new node
-        if (openSet.indexOf(connectedCell) < 0) {
-          openSet.push(connectedCell)
-        }
-        else if (tentative_gScore >= gScore.get(connectedCell)) {
-          return;		// This is not a better path.
-        }
+  //       // Discover a new node
+  //       if (openSet.indexOf(connectedCell) < 0) {
+  //         openSet.push(connectedCell)
+  //       }
+  //       else if (tentative_gScore >= gScore.get(connectedCell)) {
+  //         return;		// This is not a better path.
+  //       }
 
-        // This path is the best until now. Record it!
-        cameFrom.set(connectedCell, current);
-        gScore.set(connectedCell, tentative_gScore);
-        fScore.set(connectedCell, gScore.get(connectedCell) + this.heuristicCostEstimate(connectedCell, finishingCell));
-      });
-      // console.log('``````````````````````Looooooooop iteration end ```````````````````````');
-    }
-  }
+  //       // This path is the best until now. Record it!
+  //       cameFrom.set(connectedCell, current);
+  //       gScore.set(connectedCell, tentative_gScore);
+  //       fScore.set(connectedCell, gScore.get(connectedCell) + this.heuristicCostEstimate(connectedCell, finishingCell));
+  //     });
+  //     // console.log('``````````````````````Looooooooop iteration end ```````````````````````');
+  //   }
+  // }
 
-  getBestCellOption(openCells: Grid2DCell[]): Grid2DCell {
-    openCells.sort((a, b) => {
-      return a.getMovementWeight() - b.getMovementWeight();
-    });
-    return openCells[0];
-  }
+  // getBestCellOption(openCells: Grid2DCell[]): Grid2DCell {
+  //   openCells.sort((a, b) => {
+  //     return a.getMovementWeight() - b.getMovementWeight();
+  //   });
+  //   return openCells[0];
+  // }
 
-  reconstructPath(cameFrom: Map<Grid2DCell, any>, current: Grid2DCell) {
-    let total_path = [current];
-    while (cameFrom.has(current)) {
-      current = cameFrom.get(current);
-      total_path.push(current);
-      if (current.cellType === 'start') {
-        break;
-      }
-    }
-    return total_path;
-  }
+  // reconstructPath(cameFrom: Map<Grid2DCell, any>, current: Grid2DCell) {
+  //   let total_path = [current];
+  //   while (cameFrom.has(current)) {
+  //     current = cameFrom.get(current);
+  //     total_path.push(current);
+  //     if (current.cellType === 'start') {
+  //       break;
+  //     }
+  //   }
+  //   return total_path;
+  // }
 
-  // this is the birds eye view of distance to a target
-  heuristicCostEstimate(from: Grid2DCell, to: Grid2DCell): number {
-    const fromRow = Math.trunc(from.gridIndex / this.gridRows);
-    const fromCol = Math.trunc(from.gridIndex % this.gridCols);
-    const toRow = Math.trunc(to.gridIndex / this.gridRows);
-    const toCol = Math.trunc(to.gridIndex % this.gridCols);
-    // console.log(`from rows: ${fromRow}, from cols: ${fromCol}`);
-    // console.log(`to rows: ${toRow}, yo cols: ${toCol}`);
-    // add 1 since indicies are 0 based and pythag requires n > 0
-    const computedRow = Math.abs(fromRow - toRow) + 1;
-    const computedCol = Math.abs(fromCol - toCol) + 1;
-    // console.log(`comp rows: ${computedRow}, comp cols: ${computedCol}`);
-    // use pythagorean theorem to compute straight distance
-    const distanceTo = Math.sqrt((computedRow * computedRow) + (computedCol * computedCol));
-    return distanceTo;
-  }
+  // // this is the birds eye view of distance to a target
+  // heuristicCostEstimate(from: Grid2DCell, to: Grid2DCell): number {
+  //   const fromRow = Math.trunc(from.gridIndex / this.gridRows);
+  //   const fromCol = Math.trunc(from.gridIndex % this.gridCols);
+  //   const toRow = Math.trunc(to.gridIndex / this.gridRows);
+  //   const toCol = Math.trunc(to.gridIndex % this.gridCols);
+  //   // console.log(`from rows: ${fromRow}, from cols: ${fromCol}`);
+  //   // console.log(`to rows: ${toRow}, yo cols: ${toCol}`);
+  //   // add 1 since indicies are 0 based and pythag requires n > 0
+  //   const computedRow = Math.abs(fromRow - toRow) + 1;
+  //   const computedCol = Math.abs(fromCol - toCol) + 1;
+  //   // console.log(`comp rows: ${computedRow}, comp cols: ${computedCol}`);
+  //   // use pythagorean theorem to compute straight distance
+  //   const distanceTo = Math.sqrt((computedRow * computedRow) + (computedCol * computedCol));
+  //   return distanceTo;
+  // }
 
   indexWithinLimits(index: number): boolean {
     return (index > -1 && index < this.totalCells)
