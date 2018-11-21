f60b79d6bc67b1e33731ec1f0d13240853222e95
Treee <jonathon.lindemann@gmail.com>
2018-11-20 17:52:46 -0700

    adding astar to index exports

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Core Mod Values
 export { CoreMod } from './dist/core/core';
 
+// Algorithms
+export { AStar } from './dist/algorithms/a-star';
+
 //Data Structures
-export { Grid2D, Grid2DCell } from './dist/data-structures/grid-2d';
+export { Grid2D } from './dist/data-structures/grid-2d';
+export { Grid2DCell } from './dist/data-structures/grid-2d-cell';
 
 // Geometries
 export { BoxGeometry } from './dist/geometries/box-geometry';
