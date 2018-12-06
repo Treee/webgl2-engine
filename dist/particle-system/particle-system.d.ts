ba40ff3e823f9c84367d5c62195dcea28430991f
Treee <jonathon.lindemann@gmail.com>
2018-12-06 14:32:36 -0700

    0.0.20

--- dist/particle-system/particle-system.d.ts
+++ dist/particle-system/particle-system.d.ts
@@ -5,6 +5,7 @@ export declare class ParticleSystem {
     particles: Particle[];
     constructor(numberOfParticles: number, gl: WebGL2RenderingContext, shaderProgram: WebGLProgram);
     initializeParticles(numberOfParticles: number, gl: WebGL2RenderingContext, shaderProgram: WebGLProgram): void;
+    updateParticles(dt: number): void;
     draw(dt: number, gl: WebGL2RenderingContext, transformUniformLocation: any, colorUniformLocation: any, projectionMatrix: Mat3): void;
     randomInt(range: number): number;
 }
--- dist/particle-system/particle-system.js
+++ dist/particle-system/particle-system.js
@@ -16,10 +16,15 @@ class ParticleSystem {
             x = -1 + 2 * +(Math.random().toFixed(2));
             y = -1 + 2 * +(Math.random().toFixed(2));
             z = -1 + 2 * +(Math.random().toFixed(2));
-            const particle = new particle_1.Particle(new vec3_1.Vec3(x, y, 0), new vec3_1.Vec3(Math.cos(x), Math.sin(y), x), new vec4_1.Vec4(x, y, z, 1), this.randomInt(5));
+            const particle = new particle_1.Particle(new vec3_1.Vec3(x, y, 0), new vec3_1.Vec3(Math.cos(x), Math.sin(y), x), new vec4_1.Vec4(x, y, z, 1), 3);
             particle.createVertexArrayObject(gl, shaderProgram);
             this.particles.push(particle);
         }
+    }
+    updateParticles(dt) {
+        this.particles.forEach((particle) => {
+            particle.update(dt);
+        });
     }
     draw(dt, gl, transformUniformLocation, colorUniformLocation, projectionMatrix) {
         this.particles.forEach((particle) => {
