594ca8e8019cf22ca34da663135bf9aa661334c8
Treee <jonathon.lindemann@gmail.com>
2018-12-07 18:44:23 -0700

    particle dispersion is more spherical

--- src/particle-system/particle-system.ts
+++ src/particle-system/particle-system.ts
@@ -15,15 +15,19 @@ export class ParticleSystem {
   initializeParticles(position: Vec3, numberOfParticles: number, gl: WebGL2RenderingContext, shaderProgram: WebGLProgram) {
     let x, y;
     let z = 0;
-    let step = Math.PI;
+    let step = Math.PI / 7;
+    const twoPi = Math.PI * Math.PI;
     for (let i = 0; i < numberOfParticles; i++) {
+      if (step > twoPi) {
+        step = (step - twoPi);
+      }
       x = -1 + 2 * +(Math.random().toFixed(2));
       y = -1 + 2 * +(Math.random().toFixed(2));
       z = -1 + 2 * +(Math.random().toFixed(2));
-      const velocity = new Vec3(Math.cos(x * step), Math.sin(y * step), x);
+      const velocity = new Vec3(x * Math.cos(step), y * Math.sin(step), x);
       step += step;
       const color = new Vec4(x, y, z, 1);
-      const decay = this.randomInt(7);
+      const decay = 3;
       const particle = new Particle(position, velocity, color, decay);
       particle.createVertexArrayObject(gl, shaderProgram);
       this.particles.push(particle);
@@ -38,7 +42,9 @@ export class ParticleSystem {
 
   draw(gl: WebGL2RenderingContext, transformUniformLocation: any, colorUniformLocation: any, projectionMatrix: Mat3) {
     this.particles.forEach((particle) => {
-      particle.draw(gl, transformUniformLocation, colorUniformLocation, projectionMatrix);
+      if (particle.isActive) {
+        particle.draw(gl, transformUniformLocation, colorUniformLocation, projectionMatrix);
+      }
     });
   }
   randomInt(range: number): number {
