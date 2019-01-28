8daa76b0210f621b04358f3c2dc88c6aedbe1947
Treee <jonathon.lindemann@gmail.com>
2019-01-28 15:52:28 -0700

    adding color to box geometry initialization

--- src/geometries/box-geometry.ts
+++ src/geometries/box-geometry.ts
@@ -1,15 +1,16 @@
 import { Vec3 } from '../math/vec3';
 import { Renderable } from './renderable';
+import { Vec4 } from '../math/vec4';
 
 export class BoxGeometry extends Renderable {
 
     constructor(gl: WebGL2RenderingContext, shaderProgram: WebGLProgram) {
         super();
-        this.createRectangle(gl, new Vec3(), 25, 25);
+        this.createRectangle(gl, new Vec3(), 25, 25, new Vec4(1, 0, 0, 1));
         this.createVertexArrayObject(gl, shaderProgram);
     }
 
-    private createRectangle(gl: WebGL2RenderingContext, position: Vec3, width: number, height: number) {
+    private createRectangle(gl: WebGL2RenderingContext, position: Vec3, width: number, height: number, color: Vec4) {
         // let tempCenter = new Vec3(-(width) / 2, -(height) / 2, 1);
         // position.add(tempCenter.clone());
         this.translate(position);
@@ -25,6 +26,7 @@ export class BoxGeometry extends Renderable {
             x2, y1,
             x2, y2
         ];
+        this.color = color;
         this.geometryData.setData(gl.FLOAT, gl.TRIANGLES, false, 2, 0, 0, (this.vertices.length / 2));
         // this.geometryData = new GeometryData(gl.FLOAT, gl.TRIANGLES, false, 2, 0, 0, this.vertices.length / 2);
         // this.setCenter(-(width) / 2, -(height) / 2, 1);
@@ -32,7 +34,7 @@ export class BoxGeometry extends Renderable {
     }
 
     private createRandomRectangle(gl: WebGL2RenderingContext, position: Vec3, maxWidth: number, maxHeight: number) {
-        this.createRectangle(gl, position, this.randomInt(maxWidth), this.randomInt(maxHeight));
+        this.createRectangle(gl, position, this.randomInt(maxWidth), this.randomInt(maxHeight), new Vec4(Math.random(), Math.random(), Math.random(), 1));
     }
 
     randomInt(range: number): number {
