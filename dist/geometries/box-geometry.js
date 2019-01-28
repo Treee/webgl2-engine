78e4934201d4040287031ed535a1a7eebee7d2d6
Treee <jonathon.lindemann@gmail.com>
2019-01-28 15:52:49 -0700

    0.0.38

--- dist/geometries/box-geometry.js
+++ dist/geometries/box-geometry.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const vec3_1 = require("../math/vec3");
 const renderable_1 = require("./renderable");
+const vec4_1 = require("../math/vec4");
 class BoxGeometry extends renderable_1.Renderable {
     constructor(gl, shaderProgram) {
         super();
-        this.createRectangle(gl, new vec3_1.Vec3(), 25, 25);
+        this.createRectangle(gl, new vec3_1.Vec3(), 25, 25, new vec4_1.Vec4(1, 0, 0, 1));
         this.createVertexArrayObject(gl, shaderProgram);
     }
-    createRectangle(gl, position, width, height) {
+    createRectangle(gl, position, width, height, color) {
         // let tempCenter = new Vec3(-(width) / 2, -(height) / 2, 1);
         // position.add(tempCenter.clone());
         this.translate(position);
@@ -24,13 +25,14 @@ class BoxGeometry extends renderable_1.Renderable {
             x2, y1,
             x2, y2
         ];
+        this.color = color;
         this.geometryData.setData(gl.FLOAT, gl.TRIANGLES, false, 2, 0, 0, (this.vertices.length / 2));
         // this.geometryData = new GeometryData(gl.FLOAT, gl.TRIANGLES, false, 2, 0, 0, this.vertices.length / 2);
         // this.setCenter(-(width) / 2, -(height) / 2, 1);
         // this.setCenter(tempCenter.x, tempCenter.y, tempCenter.z);
     }
     createRandomRectangle(gl, position, maxWidth, maxHeight) {
-        this.createRectangle(gl, position, this.randomInt(maxWidth), this.randomInt(maxHeight));
+        this.createRectangle(gl, position, this.randomInt(maxWidth), this.randomInt(maxHeight), new vec4_1.Vec4(Math.random(), Math.random(), Math.random(), 1));
     }
     randomInt(range) {
         return Math.floor(Math.random() * range);
