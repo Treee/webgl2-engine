a5d1b090185d41b930ed49154371147ec2dbbd4b
Treee <jonathon.lindemann@gmail.com>
2019-02-02 18:58:19 -0700

    attempting to add color to cube

--- src/geometries/cone.ts
+++ src/geometries/cone.ts
@@ -1,11 +1,16 @@
 import { RenderableObject } from "./renderable-object";
-import { ProgramInfo, primitives, createVAOFromBufferInfo } from "twgl.js";
+import { ProgramInfo, primitives, createBufferInfoFromArrays, createVAOFromBufferInfo } from "twgl.js";
 
 export class Cone extends RenderableObject {
   constructor(gl: WebGL2RenderingContext, progInfo: ProgramInfo, uniforms: any) {
     super();
     this.alias = 'cone';
     let coneBufferInfo = primitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
+    let attr = {
+      a_color: [1, 0, 0, 1]
+    }
+    coneBufferInfo = createBufferInfoFromArrays(gl, attr, coneBufferInfo);
+
     this.bufferInfo = coneBufferInfo;
     this.programInfo = progInfo;
     this.vertexArray = createVAOFromBufferInfo(gl, progInfo, coneBufferInfo) as unknown as WebGLVertexArrayObject;
