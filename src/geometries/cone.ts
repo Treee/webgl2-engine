25f7a801c0075fe573131cb7b64b2bd7f9aae139
Treee <jonathon.lindemann@gmail.com>
2019-03-17 19:55:47 -0600

    making cone drawn by me

--- src/geometries/cone.ts
+++ src/geometries/cone.ts
@@ -1,17 +1,39 @@
 import { RenderableObject } from "./renderable-object";
-import { ProgramInfo, primitives, createVAOFromBufferInfo } from "twgl.js";
+import { ProgramInfo, createBufferInfoFromArrays, createVAOFromBufferInfo } from "twgl.js";
 
 export class Cone extends RenderableObject {
 
+  coneHeight: number = 1;
+  baseDiameter: number = 1;
+  numConeSides: number = 16;
+
   defaultUniforms = {
-    u_colorMult: [0.5, 0.5, 1, 1],
+    u_colorMult: [1, 1, 1, 1],
     u_matrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
   };
 
   constructor(gl: WebGL2RenderingContext, progInfo: ProgramInfo, uniforms: any) {
     super();
     this.alias = 'cone';
-    let coneBufferInfo = primitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
+
+    let conePositions = [0, this.coneHeight, 0]; // start with the triangle top
+    let coneIndices = [];
+    let coneColors = [];
+    let theta = (2 * Math.PI) / this.numConeSides;
+    for (let coneSide = 1; coneSide < this.numConeSides; coneSide++) {
+      let leftBaseTheta = theta * coneSide;
+      let rightBase = theta * (coneSide + 1);
+      conePositions.push(Math.cos(leftBaseTheta), 0, Math.sin(leftBaseTheta), Math.cos(rightBase), 0, Math.sin(rightBase));
+      coneIndices.push(0, coneSide, coneSide + 1);
+      coneColors.push(1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1);
+    }
+
+    let arrays = {
+      position: coneIndices,
+      color: coneColors,
+      indices: coneIndices
+    }
+    let coneBufferInfo = createBufferInfoFromArrays(gl, arrays);
 
     this.bufferInfo = coneBufferInfo;
     this.programInfo = progInfo;
