1f90709eae47dcd99aa1f2e09c0b737bfe7c8e40
Treee <jonathon.lindemann@gmail.com>
2019-03-24 19:38:11 -0600

    0.0.87

--- dist/renderer/camera/camera.js
+++ dist/renderer/camera/camera.js
@@ -71,7 +71,8 @@ class Camera {
             this.xAngle = this.xAngle + this.twoPi;
         }
         //yaw the given angle over the y unit vector
-        this.xRotation = this.xRotation.setFromAxisAngle(new three_1.Vector3(0, 1, 0), this.xAngle);
+        // this.xRotation = this.xRotation.setFromAxisAngle(new Vector3(0, 1, 0), this.xAngle);
+        this.xRotation = this.xRotation.setFromAxisAngle(new three_1.Vector3(0, 1, 0), (this.xAngle * (180 / this.pi)));
     }
     rotateForward() {
     }
