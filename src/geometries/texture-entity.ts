b4d677fc5e0f02a2eb34dc97de5ccf95a3ef44cb
Treee <jonathon.lindemann@gmail.com>
2019-04-02 16:47:35 -0600

    texture change

--- src/geometries/texture-entity.ts
+++ src/geometries/texture-entity.ts
@@ -1,5 +1,5 @@
 import { RenderableObject } from "./renderable-object";
-import { ProgramInfo, createBufferInfoFromArrays, createVAOFromBufferInfo, createTexture, TextureOptions } from "twgl.js";
+import { ProgramInfo, createBufferInfoFromArrays, createVAOFromBufferInfo, createTexture, TextureOptions, setUniforms, drawBufferInfo } from "twgl.js";
 
 export class TextureEntity extends RenderableObject {
 
@@ -9,39 +9,62 @@ export class TextureEntity extends RenderableObject {
   defaultUniforms = {
     u_colorMult: [1, 1, 1, 1],
     u_matrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    u_texture: {}
   };
+
+  texture = {};
 
   constructor(gl: WebGL2RenderingContext, progInfo: ProgramInfo, uniforms: any) {
     super();
     this.alias = 'texture-entity';
-
     let arrays = {
-      position: [
-        -this.xAxisRange, 0, -this.zAxisRange, // back left corner
-        this.xAxisRange, 0, -this.zAxisRange,  // back right corner
-        -this.xAxisRange, 0, this.zAxisRange,  // front left corner
-        this.xAxisRange, 0, this.zAxisRange    // front right corner
-      ],
-      color: [
-        0, 1, 0, 1, // back left corner
-        0, 1, 0, 1, // back right corner
-        0, 0, 1, 1, // front left corner
-        0, 0, 1, 1  // front right corner
-      ],
-      indices: [0, 2, 1, 1, 2, 3] // counter clock-wise is front face
-    }
+      position: { numComponents: 3, data: [
+            -this.xAxisRange, 0, -this.zAxisRange, // back left corner
+            this.xAxisRange, 0, -this.zAxisRange,  // back right corner
+            -this.xAxisRange, 0, this.zAxisRange,  // front left corner
+            this.xAxisRange, 0, this.zAxisRange    // front right corner
+          ]},
+      texcoord: {numComponents: 2, data: [0, 0, 0, 1, 1, 0, 1, 1]},
+      normal: {numComponents: 3, data: [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1]},
+      indices: {numComponents: 3, data: [0, 1, 2, 1, 2, 3]}
+    };
+
+
+
+
+    // let arrays = {
+    //   position: [
+    //     -this.xAxisRange, 0, -this.zAxisRange, // back left corner
+    //     this.xAxisRange, 0, -this.zAxisRange,  // back right corner
+    //     -this.xAxisRange, 0, this.zAxisRange,  // front left corner
+    //     this.xAxisRange, 0, this.zAxisRange    // front right corner
+    //   ],
+    //   color: [
+    //     0, 1, 0, 1, // back left corner
+    //     0, 1, 0, 1, // back right corner
+    //     0, 0, 1, 1, // front left corner
+    //     0, 0, 1, 1  // front right corner
+    //   ],
+    //   indices: [0, 2, 1, 1, 2, 3] // counter clock-wise is front face
+    // }
 
     let planeBufferInfo = createBufferInfoFromArrays(gl, arrays);
 
-    this.defaultUniforms.u_texture = createTexture(gl, {
-      test1: { src: '../assets/images/texture-test.jpg', mag: gl.NEAREST }
+    this.texture = createTexture(gl, {
+      test1: { src: '../assets/images/texture-test.jpg' }
     } as TextureOptions);
 
     this.bufferInfo = planeBufferInfo;
     this.programInfo = progInfo;
     this.vertexArray = createVAOFromBufferInfo(gl, progInfo, planeBufferInfo) as unknown as WebGLVertexArrayObject;
     this.uniforms = Object.assign({}, this.defaultUniforms, uniforms);
+  }
+
+  draw(gl: WebGL2RenderingContext) {
+    let programInfo = this.programInfo;
+    gl.useProgram(programInfo.program);
+    gl.bindVertexArray(this.vertexArray);
+    setUniforms(programInfo, this.uniforms);    
+    drawBufferInfo(gl, this.bufferInfo);
   }
 
   rotate(dt: number) {
