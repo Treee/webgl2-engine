0b181d156ddd6fab991a2d6c073d155c60379008
Treee <jonathon.lindemann@gmail.com>
2019-04-16 10:59:00 -0600

    removing uneeded files. shader manager is now using twgl to create shaders and link them

--- src/renderer/shaders/shader-bound-variables.ts
+++
@@ -1,6 +0,0 @@
-export class BasicShaderVariables {
-  u_transform!: WebGLUniformLocation | null;
-  u_color!: WebGLUniformLocation | null;
-
-  constructor() { }
-}
--- src/renderer/shaders/shader-manager.ts
+++ src/renderer/shaders/shader-manager.ts
@@ -1,26 +1,23 @@
-import { ShaderProgram } from './shader-program';
-import { ProgramInfoTree } from './program-info';
+import { VertexShaderType, FragmentShaderType } from './shader-types';
+
+import * as twgl from 'twgl.js';
+import { VertexShader } from './vertex-shader';
+import { FragmentShader } from './fragment-shader';
+
 export class ShaderManager {
 
-  programs: Map<string, ProgramInfoTree>;
+  programs: Map<string, twgl.ProgramInfo>;
+  vertexShader: VertexShader;
+  fragmentShader: FragmentShader;
 
   constructor() {
-    this.programs = new Map<string, ProgramInfoTree>();
+    this.programs = new Map<string, twgl.ProgramInfo>();
+    this.vertexShader = new VertexShader();
+    this.fragmentShader = new FragmentShader();
   }
 
   public initializeShaderPrograms(gl: WebGL2RenderingContext) {
-    // create the default shader program for a 2d program
-    const shaderProgram = new ShaderProgram();
-    const basicProgramInfo = new ProgramInfoTree();
-    basicProgramInfo.program = shaderProgram.getBasic2dProgram(gl);
-    basicProgramInfo.setUniforms(gl, ['u_transform', 'u_color']);
-    this.programs.set('basic-shader', basicProgramInfo);
-
-
-    const basicParticleProgramInfo = new ProgramInfoTree();
-    basicParticleProgramInfo.program = shaderProgram.getBasicParticleProgram(gl);
-    basicParticleProgramInfo.setUniforms(gl, ['u_transform', 'u_color']);
-    this.programs.set('basic-particle-shader', basicParticleProgramInfo);
+    this.programs.set('basic-shader', this.initializeBasicShader(gl));
   }
 
   public getShader(shaderKey: string): WebGLProgram {
@@ -32,5 +29,14 @@ export class ShaderManager {
       throw new Error('Shader program does not exist.');
     }
     return shaderProgram.program;
+  }
+
+  private initializeBasicShader(gl: WebGL2RenderingContext) {
+    return this.initializeShaderProgram(gl, this.vertexShader.getVertexShaderCode(VertexShaderType.TWO_D), this.fragmentShader.getfragmentShaderCode(FragmentShaderType.PASS_THROUGH));
+  }
+
+  private initializeShaderProgram(gl: WebGL2RenderingContext, vertexShader: string, fragmentShader: string, attributePrefix: string = 'a_'): twgl.ProgramInfo {
+    twgl.setAttributePrefix(attributePrefix);
+    return twgl.createProgramInfo(gl, [vertexShader, fragmentShader]);
   }
 }
