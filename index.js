2d1bb030b0199c17d5b832964ec4602fb9d70b93
Treee <jonathon.lindemann@gmail.com>
2019-04-16 15:48:17 -0600

    fixing index.js

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ export { AStar } from './dist/algorithms/a-star';
 export { Grid2D } from './dist/data-structures/grid-2d';
 export { Grid2DCell } from './dist/data-structures/grid-2d-cell';
 
-// Geometries
-export { TextureEntity } from './dist/geometries/texture-entity';
-
 // Math
 export { degreesToRadian } from './dist/math/helper';
-
-// Particle System
-
-// Shaders
-export { ShaderProgram } from './dist/renderer/shaders/shader-program';
 
 export { RendererEngine } from './dist/renderer/renderer-engine';
