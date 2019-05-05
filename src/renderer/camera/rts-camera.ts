47e9ece5742077c82a94677aad2d3f5815e835c5
Treee <jonathon.lindemann@gmail.com>
2019-05-05 17:20:56 -0600

    changed the quat multiplication from using the deprecated function

--- src/renderer/camera/rts-camera.ts
+++ src/renderer/camera/rts-camera.ts
@@ -9,8 +9,12 @@ export class RtsCamera extends Camera {
     }
 
     getForward(): v3.Vec3 {
-        const right = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), 1.5708).multiplyVector3(this.getRight());
-        return right;
+        const forward = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), 1.5708);
+        const oldRight = this.getRight();
+        const converted = new Vector3(oldRight[0], oldRight[1], oldRight[2]);
+        const forwardVec = converted.applyQuaternion(forward);
+        const convertedVec = [forwardVec.x, forwardVec.y, forwardVec.z];
+        return convertedVec;
     }
 
     // getUp(): v3.Vec3 {
