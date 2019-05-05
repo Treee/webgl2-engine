a186600d1ac6476fbbc47a21a877e7bc7c7a8eeb
Treee <jonathon.lindemann@gmail.com>
2019-05-05 17:14:11 -0600

    0.0.145

--- dist/renderer/camera/rts-camera.d.ts
+++ dist/renderer/camera/rts-camera.d.ts
@@ -2,4 +2,5 @@ import { Camera } from "./camera";
 import { v3 } from "twgl.js";
 export declare class RtsCamera extends Camera {
     constructor(startPosition?: v3.Vec3, width?: number, height?: number, near?: number, far?: number);
+    getForward(): v3.Vec3;
 }
--- dist/renderer/camera/rts-camera.js
+++ dist/renderer/camera/rts-camera.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const camera_1 = require("./camera");
+const three_1 = require("three");
 class RtsCamera extends camera_1.Camera {
     constructor(startPosition = [0, 0, 0], width = 1366, height = 768, near = 1, far = 100) {
         super(startPosition);
         this.setDiametricProjection(0, width, 0, height, near, far);
+    }
+    getForward() {
+        const right = new three_1.Quaternion().setFromAxisAngle(new three_1.Vector3(0, 1, 0), 1.5708).multiplyVector3(this.getRight());
+        return right;
     }
 }
 exports.RtsCamera = RtsCamera;
