a975737286185f12bfb5db5985b6f4498fa7edbb
Treee <jonathon.lindemann@gmail.com>
2019-05-05 17:13:50 -0600

    first attempt at modifying the forward vector of the rts camera

--- src/renderer/camera/rts-camera.ts
+++ src/renderer/camera/rts-camera.ts
@@ -1,5 +1,6 @@
 import { Camera } from "./camera";
 import { v3 } from "twgl.js";
+import { Quaternion, Vector3 } from "three";
 
 export class RtsCamera extends Camera {
     constructor(startPosition: v3.Vec3 = [0, 0, 0], width: number = 1366, height: number = 768, near: number = 1, far: number = 100) {
@@ -7,10 +8,10 @@ export class RtsCamera extends Camera {
         this.setDiametricProjection(0, width, 0, height, near, far);
     }
 
-    // getForward(): v3.Vec3 {
-    //     const right = this.getRight();
-    //     return [right[0], -right[1], right[2]];
-    // }
+    getForward(): v3.Vec3 {
+        const right = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), 1.5708).multiplyVector3(this.getRight());
+        return right;
+    }
 
     // getUp(): v3.Vec3 {
     //     return [0, 0, 0];
