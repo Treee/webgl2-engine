43cbf1307c67b882d26f90aaca6c234a893ed90d
Treee <jonathon.lindemann@gmail.com>
2020-01-08 14:11:06 -0700

    rmeoving dependencies and custom build steps

--- dist/renderer/shaders/shader-manager.js
+++ dist/renderer/shaders/shader-manager.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const shader_types_1 = require("./shader-types");
-const twgl = require("twgl.js");
+const twgl = __importStar(require("twgl.js"));
 const vertex_shader_1 = require("./vertex-shader");
 const fragment_shader_1 = require("./fragment-shader");
 class ShaderManager {
